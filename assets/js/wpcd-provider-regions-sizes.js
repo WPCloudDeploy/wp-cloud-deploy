b66037912244d19d66dbfff3ca9895e8209dbc7f
Bhargav Bhanderi <bhargav@creolestudios.com>
2022-10-03 21:05:59 +0530

    Phase 31 b changes for regions & size

--- assets/js/wpcd-provider-regions-sizes.js
+++ assets/js/wpcd-provider-regions-sizes.js
@@ -22,5 +22,27 @@
                 });
             }
         });
+
+        // change regions when provider changes for bulk intall.
+        $('body').delegate('select[name="wpcd_bulk_installs_provider[]"]', 'change', function (e) {
+
+            var bulk_install_provider = $(this);
+
+            if (Object.keys(attributes.provider_regions).length > 0) {
+               
+                $(bulk_install_provider).closest('.wpcd-bulk-installs-form-fields').find('select[name="wpcd_bulk_installs_region[]"]').empty();
+                $regions = attributes.provider_regions[$(this).val()];
+                $.each($regions, function (i, j) {
+                    $(bulk_install_provider).closest('.wpcd-bulk-installs-form-fields').find('select[name="wpcd_bulk_installs_region[]"]').append('<option value="' + j.slug + '">' + j.name + '</option>');
+                });
+            }
+            if (Object.keys(attributes.provider_sizes).length > 0) {
+                $(bulk_install_provider).closest('.wpcd-bulk-installs-form-fields').find('select[name="wpcd_bulk_installs_size[]"]').empty();
+                $sizes = attributes.provider_sizes[$(this).val()];
+                $.each($sizes, function (i, j) {
+                    $(bulk_install_provider).closest('.wpcd-bulk-installs-form-fields').find('select[name="wpcd_bulk_installs_size[]"]').append('<option value="' + j.slug + '">' + j.name + '</option>');
+                });
+            }
+        });
     }
 })(jQuery, attributes);

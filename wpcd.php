a9861baf649fd23aad26056b9a7973ce64426321
Nigel Bahadur <elindydotcom@hotmail.com>
2023-05-04 14:46:19 -0500

    Update version number.

--- wpcd.php
+++ wpcd.php
@@ -3,7 +3,7 @@
 Plugin Name: WPCloudDeploy
 Plugin URI: https://wpclouddeploy.com
 Description: Deploy and manage cloud servers and apps from inside the WordPress Admin dashboard.
-Version: 5.2.14
+Version: 5.2.15
 Requires at least: 5.4
 Requires PHP: 7.4
 Item Id: 1493

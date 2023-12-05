b8336c5d608858c1dc471ed598d84c07aa791c12
Nigel Bahadur <elindydotcom@hotmail.com>
2023-12-05 10:25:34 -0600

    Dev: WPAPP - Change three hook names to better matchup with WP conventions.

--- includes/core/apps/class-wpcd-woocommerce.php
+++ includes/core/apps/class-wpcd-woocommerce.php
@@ -44,7 +44,7 @@ class WPCD_WOOCOMMERCE {
 		$found = false;
 		$items = $order->get_items();
 		foreach ( $items as $item ) {
-			$product_id = apply_filters( 'wpcd_does_order_contain_item_of_type', $product_id );
+			$product_id = apply_filters( 'wpcd_does_order_contain_item_of_type_product_id', $product_id );
 			$is_type    = get_post_meta( $product_id, "wpcd_app_{$item_type}_product", true );
 			if ( 'yes' === $is_type ) {
 				$found = true;
@@ -52,7 +52,7 @@ class WPCD_WOOCOMMERCE {
 			}
 		}
 
-		return $found;
+		return apply_filters( 'wpcd_does_order_contain_item_of_type', $found );
 	}
 
 	/**
@@ -76,7 +76,7 @@ class WPCD_WOOCOMMERCE {
 		$return = true;
 		$items  = $order->get_items();
 		foreach ( $items as $item ) {
-			$product_id = apply_filters( 'wpcd_does_order_suppress_thank_you_notice', $product_id );
+			$product_id = apply_filters( 'wpcd_does_order_suppress_thank_you_notice_product_id', $product_id );
 			$is_type    = get_post_meta( $product_id, "wpcd_app_{$item_type}_product", true );
 			if ( 'yes' === $is_type ) {
 				$suppress = get_post_meta( $product_id, "wpcd_app_{$item_type}_no_global_thankyou_notice", true );
@@ -87,7 +87,7 @@ class WPCD_WOOCOMMERCE {
 			}
 		}
 
-		return $return;
+		return apply_filters( 'wpcd_does_order_suppress_thank_you_notice', $return );
 	}
 
 	/**
@@ -130,13 +130,13 @@ class WPCD_WOOCOMMERCE {
 
 		$items = $order->get_items();
 		foreach ( $items as $item ) {
-			$product_id = apply_filters( 'wpcd_get_unique_products_on_order', $product_id );
+			$product_id = apply_filters( 'wpcd_get_unique_products_on_order_product_id', $product_id );
 			if ( ! in_array( $product_id, $return, true ) ) {
 				array_push( $return, $product_id );
 			}
 		}
 
-		return $return;
+		return apply_filters( 'get_unique_products_on_order', $return );
 
 	}
 

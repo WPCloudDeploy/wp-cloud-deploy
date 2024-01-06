218ef87060ba3b6b616ac75a0f073909f7a6c956
Nigel Bahadur <elindydotcom@hotmail.com>
2024-01-06 05:55:49 -0600

    Tweak: WPAPP - Add additional parameters to a few filters.

--- includes/core/apps/class-wpcd-woocommerce.php
+++ includes/core/apps/class-wpcd-woocommerce.php
@@ -45,7 +45,7 @@ class WPCD_WOOCOMMERCE {
 		$items = $order->get_items();
 		foreach ( $items as $item ) {
 			$product_id = $item->get_product_id();
-			$product_id = apply_filters( 'wpcd_does_order_contain_item_of_type_product_id', $product_id );
+			$product_id = apply_filters( 'wpcd_does_order_contain_item_of_type_product_id', $product_id, $item );
 			$is_type    = get_post_meta( $product_id, "wpcd_app_{$item_type}_product", true );
 			if ( 'yes' === $is_type ) {
 				$found = true;
@@ -78,7 +78,7 @@ class WPCD_WOOCOMMERCE {
 		$items  = $order->get_items();
 		foreach ( $items as $item ) {
 			$product_id = $item->get_product_id();
-			$product_id = apply_filters( 'wpcd_does_order_suppress_thank_you_notice_product_id', $product_id );
+			$product_id = apply_filters( 'wpcd_does_order_suppress_thank_you_notice_product_id', $product_id, $item );
 			$is_type    = get_post_meta( $product_id, "wpcd_app_{$item_type}_product", true );
 			if ( 'yes' === $is_type ) {
 				$suppress = get_post_meta( $product_id, "wpcd_app_{$item_type}_no_global_thankyou_notice", true );
@@ -133,7 +133,7 @@ class WPCD_WOOCOMMERCE {
 		$items = $order->get_items();
 		foreach ( $items as $item ) {
 			$product_id = $item->get_product_id();
-			$product_id = apply_filters( 'wpcd_get_unique_products_on_order_product_id', $product_id );
+			$product_id = apply_filters( 'wpcd_get_unique_products_on_order_product_id', $product_id, $item );
 			if ( ! in_array( $product_id, $return, true ) ) {
 				array_push( $return, $product_id );
 			}

4a379428b15f510b2ab102874662025a45d1af8d
Nigel Bahadur <elindydotcom@hotmail.com>
2024-02-16 18:30:25 -0600

    Add three new settings for site expiration (implementation details to come later).

--- includes/core/apps/wordpress-app/class-wordpress-app-settings.php
+++ includes/core/apps/wordpress-app/class-wordpress-app-settings.php
@@ -1482,6 +1482,7 @@ class WORDPRESS_APP_SETTINGS extends WPCD_APP_SETTINGS {
 				),
 				'tab'     => 'wordpress-app-sites',
 			),
+
 			array(
 				'type' => 'heading',
 				'name' => __( 'Disk Quota', 'wpcd' ),
@@ -1506,25 +1507,65 @@ class WORDPRESS_APP_SETTINGS extends WPCD_APP_SETTINGS {
 				'tooltip' => __( 'This is the quota that will be added to each new site. This will override the global quota set above for all new sites.', 'wpcd' ),
 				'tab'     => 'wordpress-app-sites',
 			),
+
+			array(
+				'type' => 'heading',
+				'name' => __( 'Disk Quota Options', 'wpcd' ),
+				'desc' => __( 'What should we do when a quota is exceeded on a site?', 'wpcd' ),
+				'tab'  => 'wordpress-app-sites',
+			),
 			array(
 				'id'      => 'wordpress_app_sites_disk_quota_enable_http_auth',
 				'type'    => 'checkbox',
-				'name'    => __( 'Password Protect Site (HTTP AUTH) When Quota Exceeded?', 'wpcd' ),
+				'name'    => __( 'Password Protect Site (HTTP AUTH)?', 'wpcd' ),
 				'tooltip' => __( 'This option will place a password popup before visitors can view it. Only an admin will be able to remove this protection.  Do not enable this option with the DISABLE option below - they do not work well together!', 'wpcd' ),
 				'tab'     => 'wordpress-app-sites',
 			),
 			array(
 				'id'      => 'wordpress_app_sites_disk_quota_disable_site',
 				'type'    => 'checkbox',
-				'name'    => __( 'Disable Site When Quota Exceeded?', 'wpcd' ),
+				'name'    => __( 'Disable Site?', 'wpcd' ),
 				'tooltip' => __( 'Disabling a site will prevent visitors from viewing it. This option is slightly different from the HTTP AUTH option above because it makes the site completely invisible to web traffic - not even a password popup.  Do not enable this option with the HTTP AUTH option above - they do not work well together!', 'wpcd' ),
 				'tab'     => 'wordpress-app-sites',
 			),
 			array(
 				'id'      => 'wordpress_app_sites_disk_quota_admin_lock_site',
 				'type'    => 'checkbox',
-				'name'    => __( 'Apply Admin Lock When Quota Exceeded?', 'wpcd' ),
+				'name'    => __( 'Apply Admin Lock?', 'wpcd' ),
 				'tooltip' => __( 'The admin lock will disable all tabs for the site. A the customer cannot manage it or reactivate it. Only an admin will be able to remove the lock.', 'wpcd' ),
+				'tab'     => 'wordpress-app-sites',
+			),
+
+			array(
+				'type'    => 'heading',
+				'name'    => __( 'Site Expiration', 'wpcd' ),
+				'desc'    => __( 'What should we do when a site is expired?', 'wpcd' ),				
+				'tab'     => 'wordpress-app-sites',
+			),
+			array(
+				'id'      => 'wordpress_app_sites_expired_enable_http_auth',
+				'type'    => 'checkbox',
+				'name'    => __( 'Password Protect Site (HTTP AUTH)?', 'wpcd' ),
+				'tooltip' => __( 'This option will place a password popup before visitors can view it. Only an admin will be able to remove this protection.  Do not enable this option with the DISABLE option below - they do not work well together!', 'wpcd' ),
+				'tab'     => 'wordpress-app-sites',
+			),
+			array(
+				'id'      => 'wordpress_app_sites_expired_disable_site',
+				'type'    => 'checkbox',
+				'name'    => __( 'Disable Site?', 'wpcd' ),
+				'tooltip' => __( 'Disabling a site will prevent visitors from viewing it. This option is slightly different from the HTTP AUTH option above because it makes the site completely invisible to web traffic - not even a password popup.  Do not enable this option with the HTTP AUTH option above - they do not work well together!', 'wpcd' ),
+				'tab'     => 'wordpress-app-sites',
+			),
+			array(
+				'id'      => 'wordpress_app_sites_expired_admin_lock_site',
+				'type'    => 'checkbox',
+				'name'    => __( 'Apply Admin Lock ?', 'wpcd' ),
+				'tooltip' => __( 'The admin lock will disable all tabs for the site. A the customer cannot manage it or reactivate it. Only an admin will be able to remove the lock.', 'wpcd' ),
+				'tab'     => 'wordpress-app-sites',
+			),
+			array(
+				'type' => 'custom_html',
+				'std'  => __( 'Important Note: The three options above are only implemented when a site is not yet expired and hits the expiration date. If an admin manually expires a site, none of these options are implemented.', 'wpcd' ),
 				'tab'     => 'wordpress-app-sites',
 			),
 
